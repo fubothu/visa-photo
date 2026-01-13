2f0c8b1de94e9bb9b03011eb6a9fa872b6a8621a
Bo Fu <fubothu@yahoo.com>
2026-01-12 19:59:37 -0800

    Change default language to English

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -67,7 +67,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export const LanguageProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [language, setLanguage] = useState<Language>('zh'); // Default to Chinese as user interacted in Chinese
+    const [language, setLanguage] = useState<Language>('en'); // Default to English
 
     return (
         <LanguageContext.Provider value={{ language, setLanguage, t: translations[language] }}>
